e7d9283f97799b3546b1ad2ff7fc9e2f74e155d9
Jonathan Davis <jonathan.davis@objectivecompany.com>
2011-03-24 15:28:11 -0600

    Added a message for invalid credit cards.

--- messages/authnet/aim.php
+++ messages/authnet/aim.php
@@ -1,7 +1,8 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 return array(
 	'card_num' => array(
-		'not_empty' => 'Please enter your card number!'
+		'not_empty' => 'Please enter your card number!',
+		'credit_card' => 'Please enter a valid card number!',
 	),
 	'start_date' => array(
 		'not_empty' => 'Please enter a start date!',
